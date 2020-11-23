83fd9042de85f27b6c24f461e849d79e55ceda79
JeanCarlos911 <jeancarlosodi@gmail.com>
2020-11-23 12:55:35 -0500

    se implementa el método dibujar

--- src/app/gui.kt
+++ src/app/gui.kt
@@ -1,26 +1,20 @@
 package app
 
-import swingRAD.setBackground
-import swingRAD.setMainBar
-import swingRAD.setProperties
+import swingRAD.*
+import java.awt.Dimension
 import javax.swing.*
 
 class GUI: JFrame() {
 
     private val tfGrado = JTextField()
     private val pIzquierdo = JPanel()
-    private val spIzquierdo = JScrollPane(
-        pIzquierdo,
-        JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-        JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED
-    )
     private val pDerecho = JPanel()
-    private val spDerecho = JScrollPane(
-        pDerecho,
-        JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-        JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED
-    )
 
+    private val tfListN = mutableListOf<JTextField>()
+    private val tfListFn = mutableListOf<JTextField>()
+    private val tfListK = mutableListOf<JTextField>() // K = Constantes
+
+    //Interfaz inicial
     init{
         val lDigitar = JLabel()
         lDigitar.setProperties(490, 61, 200, 28, "Grado de la ecuación")
@@ -36,11 +30,21 @@ class GUI: JFrame() {
 
         pIzquierdo.setProperties(0, 0, 230, 300, border = null)
 
+        val spIzquierdo = JScrollPane(
+            pIzquierdo,
+            JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+            JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED
+        )
         spIzquierdo.setProperties(390, 170, 230, 300)
         add(spIzquierdo)
 
         pDerecho.setProperties(0, 0, 230, 300, border = null)
 
+        val spDerecho = JScrollPane(
+            pDerecho,
+            JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+            JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED
+        )
         spDerecho.setProperties(640, 170, 230, 300)
         add(spDerecho)
 
@@ -59,7 +63,73 @@ class GUI: JFrame() {
     }
 
     private fun dibujar() {
+        //panel izquierdo
+        pIzquierdo.removeAll()
+
+        val lN = JLabel()
+        lN.setProperties(60, 20, 40, 28, "n", fontEcuation)
+        pIzquierdo.add(lN)
+
+        val lFn = JLabel()
+        lFn.setProperties(150, 20, 40, 28, "f(n)", fontEcuation)
+        pIzquierdo.add(lFn)
+
         val grado = tfGrado.text.toInt()
+
+        for(i in 0 until grado){
+            val tfN = JTextField()
+            tfN.setProperties(30, 60 + i*32, 80, 28)
+            tfListN.add(tfN)
+            pIzquierdo.add(tfListN[i])
+
+            val tfFn = JTextField()
+            tfFn.setProperties(120, 60 + i*32, 80, 28)
+            tfListFn.add(tfFn)
+            pIzquierdo.add(tfListFn[i])
+        }
+
+        pIzquierdo.setSize(230,
+            if(grado>7)
+                92 + grado*32
+            else
+                300
+        )
+        pIzquierdo.preferredSize = Dimension(200,
+            if(grado>7)
+                92 + grado*32
+            else
+                280
+        )
+
+        //panel derecho
+        pDerecho.removeAll()
+
+        val lCoeficientes = JLabel()
+        lCoeficientes.setProperties(60, 20, 100, 28, "Coeficientes", fontEcuation)
+        pDerecho.add(lCoeficientes)
+
+        for(i in 0 .. grado) {
+            val tfK = JTextField()
+            tfK.setProperties(70, 60 + i * 32, 80, 28)
+            tfListK.add(tfK)
+            pDerecho.add(tfListK[i])
+        }
+
+        pDerecho.setSize(230,
+            if(grado>6)
+                124 + grado*32
+            else
+                300
+        )
+        pDerecho.preferredSize = Dimension(200,
+            if(grado>7)
+                124 + grado*32
+            else
+                280
+        )
+
+        //general
+        repaint()
     }
 
     private fun calcular() {
